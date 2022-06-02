cb9d1acdc653ae0739a57f16022cb31cb451b534
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2022-06-02 21:19:55 +0200

    Tests: add initial tests

---
+++ src/SuperKlasse.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thomas\GithubActionTest;
+
+class SuperKlasse
+{
+    /**
+     * @param int $number
+     * @return int
+     */
+    public function calculate(int $number): int
+    {
+        return $number + 10;
+    }
+}
