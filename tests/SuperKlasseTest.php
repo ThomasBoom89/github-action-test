7186e15372355f6c9e3cd7aee5ad057b319f0d0f
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2022-06-02 21:25:50 +0200

    Tests: fix broken test

--- tests/SuperKlasseTest.php
+++ tests/SuperKlasseTest.php
@@ -9,6 +9,6 @@ class SuperKlasseTest extends TestCase
     public function testCalculate()
     {
         $superKlasse = new SuperKlasse();
-        $this->assertEquals(33, $superKlasse->calculate(29));
+        $this->assertEquals(39, $superKlasse->calculate(29));
     }
 }
